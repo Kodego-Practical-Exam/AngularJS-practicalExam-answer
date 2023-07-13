d82350efef9e6c4dbf1a8d0754c20336ae01aa0e
daoistcoder <deservekindness@gmail.com>
2023-07-13 21:15:15 +0800

    update housing location comp

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -8,16 +8,19 @@ import { HousingLocation } from '../housinglocation';
   imports: [CommonModule],
   template: `
     <section class="listing">
-      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
     </section>
   `,
   styleUrls: ['./housing-location.component.css'],
 })
-
 export class HousingLocationComponent {
-
   @Input() housingLocation!: HousingLocation;
-
 }
